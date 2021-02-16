c8d86ebdb24672d3d8a91ee5f66ef47f09c4e6a8
rique <henrique.oliveira.gomes@gmail.com>
2021-02-16 13:27:31 -0300

    upgrade collection steam-game-data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:setup_wizard/app/pages/%20favorite/favorite_page.dart';
 import 'package:setup_wizard/app/pages/game_genre/game_genre_page.dart';
 import 'package:setup_wizard/app/pages/game_info/game_info_page.dart';
@@ -28,7 +32,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.grey,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        initialRoute: '/homePage',
+        initialRoute: '/update',
         routes: {
           '/gameGenrePage': (context) => GameGenrePage(),
           '/gameListPaginationPage': (context) => GameListPaginationPage(),
@@ -39,7 +43,49 @@ class MyApp extends StatelessWidget {
           '/gameInfoPage': (context) => GameInfoPage(),
           '/homePage': (context) => HomePage(),
           '/favoritePage': (context) => FavoritePage(),
+          '/update': (context) => UpdateFirestoreData(),
           '/hardwareCategoryPage': (context) => null,
         });
+  }
+}
+
+class UpdateFirestoreData extends StatelessWidget{
+
+
+  final CollectionReference firestoreCollection = FirebaseFirestore.instance.collection("steam-game-data");
+
+  Future loadJsonAsset() async {
+    return await rootBundle.loadString('lib/assets/json/shuffled_df_15-02_execution.json');
+  }
+
+  Future parseJson() async {
+    final String jsonFile = await loadJsonAsset();
+    return json.decode(jsonFile);
+  }
+
+  Future<void> storageDataIntoFirestore() async {
+    final Map parsedJson = await parseJson();
+
+    for (var i = 0; i < 12634; i++){
+      var currentLine = parsedJson["$i"];
+      if(currentLine != null){
+        print(i);
+        firestoreCollection.doc("$i").update({
+          "Cluster": "${currentLine["clusters"]}",
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RawMaterialButton(
+          fillColor: Colors.orange,
+          onPressed: () => storageDataIntoFirestore(),
+        ),
+      ),
+    );
   }
 }
